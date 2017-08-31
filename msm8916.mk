85d6d1c785cc8f5dbc998681d16e2cce6e90bc7e
Artem Borisov <dedsa2002@gmail.com>
2017-08-31 16:41:25 +0300

    libshim: add camera atomic calls shim
    01-21 03:47:04.275   509   509 F libc    : CANNOT LINK EXECUTABLE "/system/bin/mm-qcamera-daemon": cannot locate symbol "android_atomic_acquire_load" referenced by "/system/vendor/lib/libmmcamera2_stats_algorithm.so"...
    
    Change-Id: Ia875a5385931d5e79bfc1a99c3d8996ac66de0df

--- msm8916.mk
+++ msm8916.mk
@@ -84,10 +84,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.msm8916 \
+    camera.device@1.0-impl \
     libbson \
+    libshim_atomic  \
     libc_util \
     libshims_camera \
-    libshim_atomic  \
     libshims_sensorlistener
 
 # Camera properties
